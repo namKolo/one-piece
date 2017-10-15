3b9ed019d923c8921bf875c71d6a304daa635788
Nam Kolo <pokemon.namkolo@gmail.com>
2017-10-15 18:24:34 +0700

    Integarte with react-router v4

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 
-import Button from './component/Button';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from 'module/Home';
+import About from 'module/About';
 
 export default class App extends Component {
-  state = {
-    color: 'red'
-  };
-
-  /*
-    Change color here, it should reload magicallay
-  */
-  handleColorUpdate = () => this.setState({ color: 'yellow' });
-
   render() {
-    const { color } = this.state;
     return (
-      <div>
-        <div style={{ color }} onClick={this.handleColorUpdate}>
-          Look good manff
+      <Router>
+        <div>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} exact />
         </div>
-        <Button onClick={this.handleColorUpdate} label="hello swordman" />
-      </div>
+      </Router>
     );
   }
 }
